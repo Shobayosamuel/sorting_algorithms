1ae5594fbe0b256fd705fa3f319bdb3367f35f31
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-21 19:10:16 +0100

    Removed print array

--- sort.h
+++ sort.h
@@ -34,20 +34,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
 /*Given printing functions*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
