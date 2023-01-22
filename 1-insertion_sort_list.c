a40b233cb56115c2ea730c9a45f20e21deb03051
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-22 16:17:19 +0100

    Corrected Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -56,3 +56,25 @@ void swap(listint_t **head, listint_t *node1, listint_t *node2)
 	if (next)
 		next->prev = node1;
 }
+listint_t *create_listint(const int *array, size_t size)
+{
+    listint_t *list;
+    listint_t *node;
+    int *tmp;
+
+    list = NULL;
+    while (size--)
+    {
+        node = malloc(sizeof(*node));
+        if (!node)
+            return (NULL);
+        tmp = (int *)&node->n;
+        *tmp = array[size];
+        node->next = list;
+        node->prev = NULL;
+        list = node;
+        if (list->next)
+            list->next->prev = list;
+    }
+    return (list);
+}
